1f46fba1f1491d2c1cdd09406302b0d807090059
Purnaa <147434929+purnaa-l@users.noreply.github.com>
2024-12-23 01:10:29 +0530

    Create AqiRepository.java

---
+++ repository/AqiRepository.java
@@ -0,0 +1,11 @@
+package com.postgresql.aqi.repository;
+
+import com.postgresql.aqi.entity.Aqi;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface AqiRepository extends JpaRepository<Aqi, Long> {
+
+}
